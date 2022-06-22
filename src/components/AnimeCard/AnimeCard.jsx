3708d0a9c6d74b929a481e2b82486b2159ad21e1
driehoeken <a.bednarski1203@gmail.com>
2022-06-22 21:16:53 +0200

    card animation and hover data

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -4,12 +4,15 @@ import "./AnimeCard.css";
 const AnimeCard = (props) => {
     return (
         <div className="anime-card">
-            <img
-                className="anime-card-cover"
-                src={props.animeData.coverImage.large}
-                alt={props.animeData.title.romaji}
-            />
+            <div className="anime-card-cover-wrapper">
+                <img
+                    className="anime-card-cover"
+                    src={props.animeData.coverImage.large}
+                    alt={props.animeData.title.romaji}
+                />
+            </div>
             <p className="anime-card-title">{props.animeData.title.romaji}</p>
+            <div className="anime-card-hover-data"></div>
         </div>
     );
 };
