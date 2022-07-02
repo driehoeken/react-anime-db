744a242b91dd74be60a3467316b1a1dd82a945ff
driehoeken <a.bednarski1203@gmail.com>
2022-07-02 15:21:30 +0200

    restructuring v2

--- src/components/AnimePage/AnimePage.jsx
+++ src/components/AnimePage/AnimePage.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./AnimePage.css";
 import parse from "html-react-parser";
-import Banner from "./Banner/Banner";
-import Information from "./Information/Information";
-import Trailer from "./Trailer/Trailer";
-import Tags from "./Tags/Tags";
-import UnderTitle from "./UnderTitle/UnderTitle";
-
+import {
+    capitalizeOnlyFirst,
+    formatAnimeFormat,
+    formatAnimeStatus,
+    minsToHoursAndMins,
+    secsToHoursAndMins,
+} from "../../misc";
 const AnimePage = () => {
     const [animeData, setAnimeData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -138,37 +139,231 @@ const AnimePage = () => {
         updateAnimeData();
     }, []);
 
-    const renderUnderTitle = () => {};
+    const Banner = (props) => {
+        if (props.src) {
+            return <img className="anime-banner" src={props.src} />;
+        }
+    };
 
-    const renderTagsToggle = () => {
-        let spoilerTags = 0;
-        animeData.tags.forEach((tag) => {
-            if (tag.isMediaSpoiler || tag.isGeneralSpoiler) spoilerTags += 1;
-        });
+    const Information = (props) => {
+        //setting all data
+        const animeData = props.data;
+        const data = [
+            {
+                info: "Format: ",
+                value: formatAnimeFormat(animeData.format),
+            },
+            {
+                info: "Status: ",
+                value: formatAnimeStatus(animeData.status),
+            },
+            {
+                info: "Romaji: ",
+                value: animeData.title.romaji,
+            },
+            {
+                info: "English: ",
+                value: animeData.title.english,
+            },
+            {
+                info: "Native: ",
+                value: animeData.title.native,
+            },
+            {
+                info: "Synonyms: ",
+                value: animeData.synonyms,
+            },
+            {
+                info: "Episodes: ",
+                value: animeData.episodes,
+            },
+            {
+                info: "Episode duration: ",
+                value: minsToHoursAndMins(animeData.duration),
+            },
+            {
+                info: "Average Score: ",
+                value:
+                    (animeData.averageScore / 10) % 1 === 0
+                        ? `${animeData.averageScore / 10}.0`
+                        : animeData.averageScore / 10,
+            },
+            {
+                info: "Premiered: ",
+                value: `${capitalizeOnlyFirst(animeData.season)} ${animeData.seasonYear}`,
+            },
+            {
+                info: "Start date: ",
+                value: animeData.startDate.year
+                    ? `${animeData.startDate.day}.${animeData.startDate.month}.${animeData.startDate.year}`
+                    : null,
+            },
+            {
+                info: "End date: ",
+                value: animeData.endDate.year
+                    ? `${animeData.endDate.day}.${animeData.endDate.month}.${animeData.endDate.year}`
+                    : null,
+            },
+        ];
+        if (animeData.nextAiringEpisode) {
+            const episode = animeData.nextAiringEpisode.episode;
+            const lefTime = secsToHoursAndMins(animeData.nextAiringEpisode.timeUntilAiring);
+            data.unshift({
+                info: "Airing ",
+                value:
+                    lefTime !== 0
+                        ? `${episode} episode in ${lefTime}`
+                        : `${episode} episode in less than hour!`,
+            });
+        }
+        //render
+        const renderValue = (value) => {
+            if (!Array.isArray(value)) {
+                return value;
+            } else {
+                let result = "";
+                value.forEach((val) => {
+                    result += `${val}, `;
+                });
+                result = result.slice(0, -2);
+                return result;
+            }
+        };
+        return (
+            <div className="anime-information">
+                {data.map((info, index) => {
+                    if (info.value && animeData !== undefined) {
+                        return (
+                            <div className="anime-info" key={index}>
+                                <span className="anime-info-type">{info.info}</span>
+                                <span className="anime-info-value">{renderValue(info.value)}</span>
+                            </div>
+                        );
+                    }
+                })}
+            </div>
+        );
+    };
 
-        const renderTagToggle = (howManyTags, name, showTags, func) => {
-            if (howManyTags > 0) {
+    const Tags = (props) => {
+        const Tag = (props) => {
+            const tag = props.tag;
+            return (
+                <div className="anime-tag">
+                    <span
+                        className={tag.isMediaSpoiler ? "tag-spoiler" : ""}
+                    >{`${tag.name} ${tag.rank}%`}</span>
+                </div>
+            );
+        };
+        const TagsToggle = (props) => {
+            let spoilerTags = 0;
+            props.tags.forEach((tag) => {
+                if (tag.isMediaSpoiler || tag.isGeneralSpoiler) spoilerTags += 1;
+            });
+
+            if (spoilerTags > 0) {
                 return (
-                    <div className="anime-tag-toggle" onClick={func}>
-                        {showTags ? "Hide" : "Show"}{" "}
-                        {howManyTags !== 1 ? `${howManyTags} ${name} tags` : `${name} tag`}
+                    <div
+                        className="anime-tag-toggle"
+                        onClick={() => {
+                            if (props.showSpoilers) props.setShowSpoilerTags(false);
+                            else props.setShowSpoilerTags(true);
+                        }}
+                    >
+                        {props.showSpoilers ? "Hide" : "Show"}{" "}
+                        {spoilerTags !== 1 ? `${spoilerTags} spoiler tags` : `spoiler tag`}
                     </div>
                 );
             }
         };
-
-        if (spoilerTags > 0) {
-            return (
-                <div>
-                    {renderTagToggle(spoilerTags, "spoiler", showSpoilerTags, () => {
-                        if (showSpoilerTags) setShowSpoilerTags(false);
-                        else setShowSpoilerTags(true);
+        const showSpoilers = props.showSpoilers;
+        return (
+            <div className="anime-tags-wrapper">
+                <h2>Tags</h2>
+                <div className="anime-tags">
+                    {props.tags.map((tag, index) => {
+                        if (
+                            ((showSpoilers === false && tag.isMediaSpoiler === false) ||
+                                showSpoilers) &&
+                            ((showSpoilers === false && tag.isGeneralSpoiler === false) ||
+                                showSpoilers)
+                        ) {
+                            return <Tag key={index} tag={tag} />;
+                        }
                     })}
                 </div>
-            );
+                <TagsToggle
+                    tags={props.tags}
+                    showSpoilers={showSpoilers}
+                    setShowSpoilerTags={props.setShowSpoilerTags}
+                />
+            </div>
+        );
+    };
+
+    const Trailer = (props) => {
+        if (props.trailer) {
+            const site = props.trailer.site;
+            const id = props.trailer.id;
+
+            if (site === "youtube") {
+                return (
+                    <div className="anime-trailer">
+                        <iframe
+                            width="560"
+                            height="315"
+                            src={`https://www.youtube.com/embed/${id}`}
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                );
+            }
         }
     };
 
+    const UnderTitle = (props) => {
+        const format = props.format;
+        const startYear = props.startYear;
+        const endYear = props.endYear;
+        const season = props.season;
+        const seasonYear = props.seasonYear;
+        const duration = props.duration;
+        const status = props.status;
+        const episodes = props.episodes;
+        const outcome = [formatAnimeFormat(format)];
+
+        if (endYear - startYear >= 2) {
+            outcome.push(`${startYear} - ${endYear}`);
+        } else {
+            outcome.push(`${capitalizeOnlyFirst(season)} ${seasonYear}`);
+        }
+
+        if (format !== "MOVIE") {
+            if (episodes) {
+                if (episodes === 1) {
+                    outcome.push(`1 episode`);
+                } else {
+                    outcome.push(`${episodes} episodes`);
+                }
+            }
+        } else {
+            outcome.push(minsToHoursAndMins(duration));
+        }
+
+        outcome.push(formatAnimeStatus(status));
+
+        return (
+            <ul className="anime-under-title">
+                {outcome.map((info, index) => {
+                    return <li key={index}>{info}</li>;
+                })}
+            </ul>
+        );
+    };
     const renderAnimePage = () => {
         if (!loading) {
             return (
--- src/components/AnimePage/Banner/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Banner.css";
-const Banner = (props) => {
-    if (props.src) {
-        return <img className="anime-banner" src={props.src} />;
-    }
-};
-
-export default Banner;
