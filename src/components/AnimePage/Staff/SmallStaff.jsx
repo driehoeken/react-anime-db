c9fd5fd0df219ab21b9fd00d9d5db276d21f30e0
driehoeken <a.bednarski1203@gmail.com>
2022-07-11 21:46:19 +0200

    staff toggle

--- src/components/AnimePage/Staff/SmallStaff.jsx
+++ src/components/AnimePage/Staff/SmallStaff.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import StaffCard from "./StaffCard.jsx";
+import { Link } from "react-router-dom";
 
 const Staff = (props) => {
     let people = props.staff;
     const edges = props.edges;
+    const { id, name } = useParams();
 
     if (people !== null && people?.length > 0) {
         people = people.slice(0, 3);
@@ -21,6 +24,9 @@ const Staff = (props) => {
                         );
                     })}
                 </div>
+                <Link to={`/anime/${id}/${name}/staff`} className="anime-staff-toggle">
+                    <p>Show all</p>
+                </Link>
             </div>
         );
     }
